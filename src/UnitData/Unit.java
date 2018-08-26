d4fcc713b13909db2998c4314023d24e2b07d0ca
Jeremy Eberhardt <Jeremy@10.0.0.111>
2018-08-26 10:07:29 -0500

    Unit Class (w/ hello world temporarily)

---
+++ src/UnitData/Unit.java
@@ -0,0 +1,10 @@
+package UnitData;
+
+public class Unit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
---
+++ src/UnitData/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Jeremy
+ *
+ */
+package UnitData;
